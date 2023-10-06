44b6f75c11455ba37b8dd0f9452bbf6ba0bcfc3f
Nathalia Valentim <nathadelino@hotmail.com>
2023-10-06 09:31:47 -0300

    finall

---
+++ projeto_inicial/admin/controller/cadastroProduto-controller.js
@@ -0,0 +1,15 @@
+// Importa o clienteService do módulo clientes-service.js.
+/*import { clienteService } from "../service/clientes-service.js"
+
+const formulario = document.querySelector('[data-form]')
+formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+
+       const produto = evento.target.querySelector('[data-produto]').value
+
+       const preco = evento.target.querySelector('[data-preco]').value
+
+       const descricao = evento.target.querySelector('[data-descricao]').value
+
+       clienteService.criarProduto(produto, preco, descricao)
+})*/
